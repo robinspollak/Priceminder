925cb46f2b8f1c822e0b83a2bdbebcff655db86d
rpollak96 <rpollak96@gmail.com>
2016-09-23 12:17:39 +0200

    fix up front end a bit, start working towards graphs

--- events/views.py
+++ events/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import DetailView, ListView
 
+from pricetracker.settings import trim_datetime
+
 from .models import Event
 
 class HomeView(ListView):
@@ -17,3 +19,29 @@ class HomeView(ListView):
 
 class EventView(DetailView):
     model = Event
+
+    def get_context_data(self, **kwargs):
+        context = super(EventView, self).get_context_data(**kwargs)
+        self.prepare_chart_data()
+        return context
+
+    def prepare_chart_data(self):
+        sections = self.object.sections.all()
+        prepared_data = []
+        data_labels = ['Date']
+        dates = []
+        for section in sections:
+            data_labels.append(section.name)
+            for pricepoint in section.pricepoints.all().order_by('datetime'):
+                dt = pricepoint.datetime.strftime("%m/%d/%Y, %I:%M%p")
+                if dt not in dates:
+                    dates.append(dt)
+        prepared_data.append(data_labels)
+        dates = sorted([[date] for date in dates])
+        for section in sections:
+            pricepoints = list(section.pricepoints.all().order_by('datetime'))
+            for i in range(len(pricepoints)):
+                dates[i].append(pricepoints[i].total_amount)
+        for date in dates:
+            prepared_data.append(date)
+        return prepared_data
