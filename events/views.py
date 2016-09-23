904a70f15202c9696f85f14191abba1ca4f6dc54
rpollak96 <rpollak96@gmail.com>
2016-09-23 10:57:40 +0200

    add homepage, base template, styles

--- events/views.py
+++ events/views.py
@@ -6,5 +6,14 @@ class HomeView(ListView):
     model = Event
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['not_on_sale'] = self.not_on_sale()
+        return context
+
+    def not_on_sale(self):
+        upcoming = ['Lollapalooza', 'Bonnaroo', "Governor's Ball", "EDC: Las Vegas", "South by Southwest"]
+        return upcoming
+
 class EventView(DetailView):
     model = Event
